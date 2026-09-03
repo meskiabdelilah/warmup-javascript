23a8b1818a90863b157053657d1c61c4e2a781ed
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-09-03 22:01:08 +0100

    Fich de pressentation et types Js

---
+++ exercice-01.js
@@ -0,0 +1,18 @@
+const prenom = "Amina";
+const ville = "Casablanca";
+let age = 18;
+let estEnFormation = true;
+
+console.log(`${prenom}, ${age} ans, habite a ${ville}`);
+
+const statut = age >= 18 ? "majeur" : "mineur";
+console.log(`Statut : ${statut}.`);
+
+console.log(`Formation en cours : ${estEnFormation}`);
+console.log(`perenom -> ${typeof prenom}
+age -> ${typeof age}
+estEnFormation -> ${typeof estEnFormation}`);
+
+
+
+
