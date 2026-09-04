52bc94089a4af49ca5101e3ed90f0ab6e83112c1
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-09-04 11:58:52 +0100

    Using indexof splice and for in Js

--- exercice-04.js
+++ exercice-04.js
@@ -1,5 +1,31 @@
 const courses = ["pain", "lait", "riz", "cafe"];
 
 courses.push('sucre');
-console.log(courses);
+// console.log(courses);
+
+const indexLait = courses.indexOf('lait');
+// console.log(indexLait);
+
+courses.splice(indexLait,1)
+// console.log(courses);
+
+console.log("Nombre d'articles : " , courses.length);
+
+// courses.forEach((item, index)=>{
+//     console.log(`${index +1}. ${item}`);
+// })
+
+let number = 0;
+for (let i = 0; i < courses.length; i++) {
+    let item = courses[i];
+    number ++
+console.log(`${number}. ${item}`);
+
+}
+
+for (let i = 0; i < courses.length; i++) {
+    if (courses[i] === "cafe") {
+        console.log("le cafe est bien dans la list");     
+    }
+}
 
